8fc8d82c1e69a7a057a5da4d5e6f48a6bb5b0260
Ankit-Shukla24 <ankitshukla2409@gmail.com>
2023-09-24 19:14:08 +0530

    unit test

--- backend/Models/Credential.cs
+++ backend/Models/Credential.cs
@@ -5,6 +5,18 @@ namespace backend.Models;
 
 public partial class Credential
 {
+    public Credential()
+    {
+    }
+
+    public Credential(string userId, string? password, int? customerId, bool? isEnabled)
+    {
+        UserId = userId;
+        Password = password;
+        CustomerId = customerId;
+        IsEnabled = isEnabled;
+    }
+
     public string UserId { get; set; } = null!;
 
     public string? Password { get; set; }
--- backend/Models/CredentialViewModel.cs
+++ backend/Models/CredentialViewModel.cs
@@ -2,6 +2,16 @@
 {
     public class CredentialViewModel
     {
+        public CredentialViewModel()
+        {
+        }
+
+        public CredentialViewModel(string userName, string? password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
         public string UserName { get; set; } 
 
         public string? Password { get; set; }
