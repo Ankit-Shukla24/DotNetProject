321322d22c744118fc0a1e902023c8ddd69d37ed
Anuj7152 <anuj20401@gmail.com>
2023-09-25 00:08:59 +0530

    Added styling to pin change page

--- atm-system/src/pages/PinChange.js
+++ atm-system/src/pages/PinChange.js
@@ -2,6 +2,9 @@ import { useState, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
 import logout from "../components/LogOut";
+import Card from "../components/Card/Card";
+import Input from "../components/Input/Input";
+import Button from "../components/Button/Button";
 
 const PinChange = () => {
   const [user, setUser] = useContext(AuthContext);
@@ -9,54 +12,48 @@ const PinChange = () => {
     OldPin: 0,
     Pin: 0,
   });
-  let token = eval(user);
-  token = token.token;
-  const headers = { "Authorization": `Bearer ` + token };
+  // let token = eval(user);
+  // token = token.token;
+  // const headers = { "Authorization": `Bearer ` + token };
   const handleChangepin = (event) => {
     setpin({ ...pin, [event.target.name]: event.target.value });
   };
 
   const handleSubmit = (event) => {
-
     event.preventDefault();
 
     console.log(pin);
 
+    // axios.post(` https://localhost:7182/api/Accounts/changePin?oldPin=${pin.OldPin}&newPin=${pin.Pin}`, {}, { headers: headers }).then((response) => {
 
-    axios.post(` https://localhost:7182/api/Accounts/changePin?oldPin=${pin.OldPin}&newPin=${pin.Pin}`, {}, { headers: headers }).then((response) => {
+    //   console.log(response);
+    //   if (response.status == 200) {
+    //     alert(response.data);
+    //   }
 
-      console.log(response);
-      if (response.status == 200) {
-        alert(response.data);
-      }
-
-    }).catch((err) => {
-      console.log(err);
-      alert(err.response.data);
-    })
-
-
-  }
-
+    // }).catch((err) => {
+    //   console.log(err);
+    //   alert(err.response.data);
+    // })
+  };
 
   return (
-    <>
+    <Card>
       <h1>Enter Pin Details</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          OldPin :
+          Old Pin
           <br />
-          <input type="number" name="OldPin" onChange={handleChangepin} />
+          <Input type="number" name="OldPin" onChange={handleChangepin} />
         </div>
         <div>
-          NewPin:
+          New Pin
           <br />
-          <input type="number" name="Pin" onChange={handleChangepin} />
+          <Input type="number" name="Pin" onChange={handleChangepin} />
         </div>
-        <button type="submit">Submit</button>
+        <Button type="submit">Submit</Button>
       </form>
-      <button onClick={logout}>LogOut</button>
-    </>
+    </Card>
   );
 };
 
