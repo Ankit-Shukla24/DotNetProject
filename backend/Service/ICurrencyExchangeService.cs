d61e1b3a46d92b5c292e768afd00687c9d779d42
Ankit-Shukla24 <ankitshukla2409@gmail.com>
2023-09-25 11:56:11 +0530

    Deletion of files

--- backend/Service/ICurrencyExchangeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using backend.Data;
-
-namespace backend.Service
-{
-    public interface ICurrencyExchangeService
-    {
-        public decimal ExchangeRates(string currency,Decimal amount);
-    }
-}
