dbb633264ce55ffb4271c4e3f5c440840d2efb8c
Ankit-Shukla24 <ankitshukla2409@gmail.com>
2023-09-26 13:02:00 +0530

    Alert msg changes

--- backend/Data/AccountDataProvider.cs
+++ backend/Data/AccountDataProvider.cs
@@ -45,7 +45,7 @@ namespace backend.Data
                 _context.Transactionhistories.Add(new Transactionhistory(account.AccountId, null, amount, DateTime.Now));
                 _context.SaveChanges();
                 transaction.Commit();
-                return account.Balance.ToString();
+                return "Balance :"+account.Balance.ToString();
 
             }
             catch (Exception ex)
@@ -78,7 +78,7 @@ namespace backend.Data
                 _context.Transactionhistories.Add(new Transactionhistory(null, account.AccountId, amount, DateTime.Now));
                 _context.SaveChanges();
                 transaction.Commit();
-                return account.Balance.ToString();
+                return "Balance :"+account.Balance.ToString();
             }
             catch (Exception ex)
             {
